5d055b65e256e21a17cb3fd0a360a0ab4b58cc6d
Karlo Sadural <karlo.sadural@gmail.com>
2025-05-17 14:10:51 -0400

    Multiplayer pt 3: Lobby Menu
    Lobby menu opens i just need to get player names showing

--- Source/TestProject/GameInst_CPP.cpp
+++ Source/TestProject/GameInst_CPP.cpp
@@ -70,13 +70,14 @@ void UGameInst_CPP::OnCreateSessionComplete(FName SessionName, bool bSuccess)
         UWorld* context_object = GetWorld();
         if (context_object)
         {
-            UGameplayStatics::OpenLevel(context_object, FName("ThirdPersonMap"), true, FString("listen"));
-			UE_LOG(LogTemp, Display, TEXT("Session created"));
+            UGameplayStatics::OpenLevel(context_object, FName("LVL_Lobby"), true, FString("listen"));
+			UE_LOG(LogTemp, Error, TEXT("Session created"));
         }
         else
         {
             UWorld* world_context_object = GEngine->GetWorldFromContextObjectChecked(this);
-            UGameplayStatics::OpenLevel(world_context_object, FName("ThirdPersonMap"), true, FString("listen"));
+            UGameplayStatics::OpenLevel(world_context_object, FName("LVL_Lobby"), true, FString("listen"));
+			UE_LOG(LogTemp, Error, TEXT("Session created, opened level"));
 
         }
     }
